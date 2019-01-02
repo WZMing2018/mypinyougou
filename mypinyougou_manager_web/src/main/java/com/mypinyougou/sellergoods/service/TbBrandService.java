d4ce60b945a039f642f1621c36da6edc24504b8f
WZMing <wangzhenming_test@163.com>
2019-01-02 21:30:06 +0800

    品牌列表AngularJS前台实现

--- mypinyougou_manager_web/src/main/java/com/mypinyougou/sellergoods/service/TbBrandService.java
+++
@@ -1,9 +0,0 @@
-package com.mypinyougou.sellergoods.service;
-
-import com.mypinyougou.pojo.TbBrand;
-
-import java.util.List;
-
-public interface TbBrandService {
-    public List<TbBrand> findAll();
-}
